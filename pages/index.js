c31849aa5474bac31c012eb9b7b9cf7be3a02288
hanasai95 <1220hanasai@gmail.com>
2021-09-15 14:24:59 -0400

    search engine and load more done, also added characters.

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ data }) {
     });
     const { current } = page;
 
-    //testing functions setup in useEffect
+
 
     useEffect(() => {
 
@@ -62,7 +62,96 @@ export default function Home({ data }) {
     }, [current]);
 
 
+    function handleLoadMore() {
+        updatePage(prev => {
+            return {
+                ...prev,
+                current: page?.next
+            }
+        });
+    }
+    function handleOnSubmitSearch(e) {
+        e.preventDefault();
+
+        const { currentTarget = {} } = e;
+        const fields = Array.from(currentTarget?.elements);
+        const fieldQuery = fields.find(field => field.name === 'query');
+
+        const value = fieldQuery.value || '';
+        const endpoint = `https://rickandmortyapi.com/api/character/?name=${value}`;
+
+        updatePage({
+            current: endpoint
+        });
+    }
     return (
-        <div>This is home page</div>
+        <div className="container">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <motion.div initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: .4
+                        }
+                    },
+                }}>
+                    <h1 className="title">
+                        Wubba Lubba Dub Dub!
+                    </h1>
+                </motion.div>
+
+
+
+                <p className="description">
+                    Rick and Morty Character Wiki
+                </p>
+
+                <form className="search" onSubmit={handleOnSubmitSearch}>
+                    <input name="query" type="search" />
+                    <button>Search</button>
+                </form>
+
+                <ul className="grid">
+                    {results.map(result => {
+                        const { id, name, image } = result;
+                        return (
+                            <motion.li key={id} className="card" whileHover={{
+
+                            }}>
+                                <Link href="/character/[id]" as={`/character/${id}`}>
+                                    <a>
+                                        <img src={image} alt={`${name} Thumbnail`} />
+                                        <h3>{name}</h3>
+                                    </a>
+                                </Link>
+                            </motion.li>
+                        )
+                    })}
+                </ul>
+
+                <p>
+                    <button onClick={handleLoadMore}>Load More</button>
+                </p>
+            </main>
+
+
+
+            <style>{
+
+            }</style>
+        </div>
     )
 }
+
+
+
