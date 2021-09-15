f2c48a530ad482f3bbd6b6c748264d57e6d236c5
hanasai95 <1220hanasai@gmail.com>
2021-09-14 21:03:01 -0400

    setting up initial project structure.

--- pages/api/character/index.js
+++ pages/api/character/index.js
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
 import Head from 'next/head';
 import Link from 'next/link';
 
 const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
 
-export async function getServerSideProps() {
-    const res = await fetch(defaultEndpoint)
+export async function getServerSideProps({ query }) {
+    const { id } = query;
+    const res = await fetch(`${defaultEndpoint}${id}`);
     const data = await res.json();
     return {
         props: {
@@ -15,6 +14,6 @@ export async function getServerSideProps() {
     }
 }
 
-export default function Home({ data }) {
+export default function Character({ data }) {
 
 }
